d438cb91544525c41137c314d991abd9faa8d720
siri <madhurisireesha6@gmail.com>
2023-10-21 11:02:03 +0530

    modify

--- src/WinOrLoseCard.js
+++ src/WinOrLoseCard.js
@@ -1,24 +1,24 @@
 import './winorlose.css'
 const WinOrLoseCard=(props)=>{
-    const{card,text,score, onPlayAgain}=props
-    const onClickPlayAgain=()=>
-    {
-        onPlayAgain()
+    const{card,text,score,playOne,topscore}=props
+    const playTwo=()=>{
+        playOne()
     }
    
-const imgur=text==='you win'?"https://assets.ccbp.in/frontend/react-js/win-game-img.png":"https://assets.ccbp.in/frontend/react-js/lose-game-img.png"
+const imgur=text==='You Win'?"https://assets.ccbp.in/frontend/react-js/win-game-img.png":"https://assets.ccbp.in/frontend/react-js/lose-game-img.png"
     return(
         <div className="winorlosecontainer">
+            
             <div className="firstwon">
                 <h1>{text}</h1>
                 <h3>Best Score</h3>
                 <p>{score}/12</p>
-               <button className='clicki' onClick={onClickPlayAgain}>play Again</button>
+               <button className='clicki' onClick={playTwo}>Play Again</button>
             </div>
             <div className="winimage">
                 <img src={imgur}/>
             </div>
-
+        
         </div>
     )
 
