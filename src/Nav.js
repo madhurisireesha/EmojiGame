1c42dd31c1e1466eef9d13b4249d45eb3cc7aabf
siri <madhurisireesha6@gmail.com>
2023-10-21 11:00:44 +0530

    modify

--- src/Nav.js
+++ src/Nav.js
@@ -1,13 +1,20 @@
 import './nav.css'
 const Nav=(props)=>{
-    const{score,topscore}=props
+    const{score,topscore,card}=props
+    
     return(
         <div className="navcontainer">
-            <div className='scoreco'>
-            <p>Score:{score}</p>
-            <p>TopScore:{topscore}</p>
+            <div className='one'>
+           
+            <img
+          src="https://assets.ccbp.in/frontend/react-js/game-logo-img.png"
+          className="img1"
+          alt="emoji logo"
+        /> <h3>Emoji Game</h3>
             </div>
-        </div>
+            {card===false&&<div className='scorecard'><p>{}</p><p>{}</p></div>}
+           {card===true&&<div className='scorecard'><p>Score:{score}</p><p>TopScore:{topscore}</p></div>} 
+         </div>
     )
 }
 export default Nav
